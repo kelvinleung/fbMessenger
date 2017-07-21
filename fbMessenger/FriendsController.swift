226110ba9d694449128e85a37bc31461b166fa0b
Kelvin <et.kelvin@gmail.com>
2017-07-21 22:42:26 +0800

    Initial Commit

---
+++ fbMessenger/FriendsController.swift
@@ -0,0 +1,50 @@
+//
+//  FriendsController.swift
+//  fbMessenger
+//
+//  Created by Kelvin Leung on 18/07/2017.
+//  Copyright © 2017 Kelvin Leung. All rights reserved.
+//
+
+import UIKit
+
+class FriendsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.alwaysBounceVertical = true
+        
+        collectionView?.register(FriendCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+}
+
+class FriendCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        backgroundColor = UIColor.yellow
+    }
+}
---
+++ fbMessenger/Notes.md
@@ -0,0 +1,11 @@
+Manuall StoryBoard
+
+```swift
+window = UIWindow(frame: UIScreen.main.bounds)
+window?.makeKeyAndVisible()
+
+let layout = UICollectionViewFlowLayout()
+let friendsController = FriendsController(collectionViewLayout: layout)
+
+window?.rootViewController = UINavigationController(rootViewController: friendsController)
+```
