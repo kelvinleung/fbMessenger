6c67dea53b13d477dfad23ace0f4e82839bc0c53
Kelvin <et.kelvin@gmail.com>
2017-07-26 22:24:50 +0800

    clear and fetch data from CoreData

--- fbMessenger/FriendsController.swift
+++ fbMessenger/FriendsController.swift
@@ -20,6 +20,7 @@ class FriendsController: UICollectionViewController, UICollectionViewDelegateFlo
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        clearData()
         setupData()
         
         navigationItem.title = "Recent"
@@ -31,10 +32,7 @@ class FriendsController: UICollectionViewController, UICollectionViewDelegateFlo
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if let count = messages?.count {
-            return count
-        }
-        return 10
+        return messages?.count ?? 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -55,23 +53,73 @@ class FriendsController: UICollectionViewController, UICollectionViewDelegateFlo
     private func setupData() {
         if let context = container?.viewContext {
             let mark = Friend(context: context)
-            mark.name = "Mark Zuckerberg"
-            mark.profileImageName = "profile"
+            mark.name = "Mark Lee"
+            mark.profileImageName = "boy"
             
-            let message = Message(context: context)
-            message.friend = mark
-            message.text = "Hello, my name is Mark. Nice to meet you..."
-            message.date = NSDate()
+            let tracy = Friend(context: context)
+            tracy.name = "Tracy Ho"
+            tracy.profileImageName = "girl"
+            
+            createMessageWithText(text: "Hello, my name is Mark", friend: mark, minutesAgo: 1, context: context)
+            createMessageWithText(text: "Would you like some coffee?", friend: mark, minutesAgo: 3, context: context)
+            createMessageWithText(text: "Hello, how are you today?", friend: tracy, minutesAgo: 7, context: context)
+            createMessageWithText(text: "I'm going to the zoo with my friends.", friend: tracy, minutesAgo: 9, context: context)
+            createMessageWithText(text: "Where are you going?", friend: tracy, minutesAgo: 13, context: context)
             
             try? context.save()
         }
         loadData()
     }
     
+    private func fetchFriends() -> [Friend]? {
+        if let context = container?.viewContext {
+            let request: NSFetchRequest<Friend> = Friend.fetchRequest()
+            return try? context.fetch(request)
+        }
+        return nil
+    }
+    
     private func loadData() {
         if let context = container?.viewContext {
-            let request:NSFetchRequest<Message> = Message.fetchRequest()
-            self.messages = try? context.fetch(request)
+            if let friends = fetchFriends() {
+                messages = [Message]()
+                for friend in friends {
+                    let request: NSFetchRequest<Message> = Message.fetchRequest()
+                    request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+                    request.predicate = NSPredicate(format: "friend.name = %@", friend.name!)
+                    request.fetchLimit = 1
+                    if let fetchedMessages = try? context.fetch(request) {
+                        messages?.append(contentsOf: fetchedMessages)
+                    }
+                }
+            }
         }
+    }
+    
+    private func clearData() {
+        if let context = container?.viewContext {
+            let messageRequest: NSFetchRequest<Message> = Message.fetchRequest()
+            let friendRequest: NSFetchRequest<Friend> = Friend.fetchRequest()
+            
+            if let messages = try? context.fetch(messageRequest) {
+                for message in messages {
+                    context.delete(message)
+                }
+            }
+
+            if let friends = try? context.fetch(friendRequest) {
+                for friend in friends {
+                    context.delete(friend)
+                }
+            }
+            try? context.save()
+        }
+    }
+    
+    private func createMessageWithText(text: String, friend: Friend, minutesAgo: Double, context: NSManagedObjectContext) {
+        let message = Message(context: context)
+        message.friend = friend
+        message.text = text
+        message.date = NSDate().addingTimeInterval(-minutesAgo * 60)
     }
 }
