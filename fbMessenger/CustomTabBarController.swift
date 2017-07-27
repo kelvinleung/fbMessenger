b7d9a8862d7f65b0c493e66c3021cbbd0e20d54a
Kelvin <et.kelvin@gmail.com>
2017-07-28 01:02:16 +0800

    add all the tab bar view controllers

--- fbMessenger/CustomTabBarController.swift
+++ fbMessenger/CustomTabBarController.swift
@@ -19,6 +19,21 @@ class CustomTabBarController: UITabBarController {
         recentMessagesNavController.tabBarItem.title = "Recent"
         recentMessagesNavController.tabBarItem.image = UIImage(named: "recent")
         
-        viewControllers = [recentMessagesNavController]
+        let callsNavController = createEmptyNavController(title: "Calls", imageName: "calls")
+        let groupsNavController = createEmptyNavController(title: "Groups", imageName: "groups")
+        let peopleNavController = createEmptyNavController(title: "People", imageName: "people")
+        let settingsNavController = createEmptyNavController(title: "Settings", imageName: "settings")
+        
+        viewControllers = [recentMessagesNavController, callsNavController, groupsNavController, peopleNavController, settingsNavController]
+    }
+    
+    private func createEmptyNavController(title: String, imageName: String) -> UINavigationController {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = UIColor.white
+        viewController.navigationItem.title = title
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        return navController
     }
 }
